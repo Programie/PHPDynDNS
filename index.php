711b56d94f3340b24b61325b31a9345d080a7e97
Michael Wieland <git@selfcoders.com>
2017-05-25 22:19:01 +0200

    Reformat code

--- index.php
+++ index.php
@@ -11,11 +11,10 @@ define("ERROR_INVALID_IP", "iperror");// IP address is invalid
 
 // Check whether the configuration file exists
 $configFile = __DIR__ . "/config.json";
-if (!file_exists($configFile))
-{
-	header("HTTP/1.1 500 Internal Server Error");
-	echo "System not configured!";
-	exit;
+if (!file_exists($configFile)) {
+    header("HTTP/1.1 500 Internal Server Error");
+    echo "System not configured!";
+    exit;
 }
 
 // Read configuration file
@@ -23,9 +22,8 @@ $config = json_decode(file_get_contents($configFile));
 $users = $config->users;
 
 // Allow Icinga/Nagios to check this application
-if (preg_match("/^check_http/", $_SERVER["HTTP_USER_AGENT"]))
-{
-	exit;
+if (preg_match("/^check_http/", $_SERVER["HTTP_USER_AGENT"])) {
+    exit;
 }
 
 // Read user provided data
@@ -35,46 +33,40 @@ $hostname = $_GET["hostname"];
 $ipAddress = $_GET["ipaddress"] ?: $_SERVER["REMOTE_ADDR"];
 
 // Check whether the given username and password match to an account
-if (!isset($users->{$username}) or $users->{$username}->password != $password)
-{
-	header("WWW-Authenticate: Basic realm=\"DynDNS Update\"");
-	header("HTTP/1.0 401 Unauthorized");
-	echo ERROR_BADAUTH;
-	exit;
+if (!isset($users->{$username}) or $users->{$username}->password != $password) {
+    header("WWW-Authenticate: Basic realm=\"DynDNS Update\"");
+    header("HTTP/1.0 401 Unauthorized");
+    echo ERROR_BADAUTH;
+    exit;
 }
 
 $user = $users->{$username};
 
 // Check whether the given hostname is valid
-if (!$hostname or !isset($user->hosts->{$hostname}))
-{
-	header("HTTP/1.1 400 Bad Request");
-	echo ERROR_INVALID_HOST;
-	exit;
+if (!$hostname or !isset($user->hosts->{$hostname})) {
+    header("HTTP/1.1 400 Bad Request");
+    echo ERROR_INVALID_HOST;
+    exit;
 }
 
 // Check whether the given IP address is valid
-if (!$ipAddress or !filter_var($ipAddress, FILTER_VALIDATE_IP))
-{
-	header("HTTP/1.1 400 Bad Request");
-	echo ERROR_INVALID_IP;
-	exit;
+if (!$ipAddress or !filter_var($ipAddress, FILTER_VALIDATE_IP)) {
+    header("HTTP/1.1 400 Bad Request");
+    echo ERROR_INVALID_IP;
+    exit;
 }
 
 // Is IPv4 address?
-if (preg_match("/^\d{1,3}(\.\d{1,3}){3,3}$/", $ipAddress))
-{
-	$entryType = "A";
-}
-else
-{
-	$entryType = "AAAA";
+if (preg_match("/^\d{1,3}(\.\d{1,3}){3,3}$/", $ipAddress)) {
+    $entryType = "A";
+} else {
+    $entryType = "AAAA";
 }
 
 // Build nsupdate commands
 $nsupdateCommands = array();
 $nsupdateCommands[] = "server " . $config->server;
-$nsupdateCommands[] = "zone " .$user->hosts->{$hostname}->zone;
+$nsupdateCommands[] = "zone " . $user->hosts->{$hostname}->zone;
 $nsupdateCommands[] = "update delete " . $hostname . " " . $entryType;
 $nsupdateCommands[] = "update add " . $hostname . " " . $config->ttl . " " . $entryType . " " . $ipAddress;
 $nsupdateCommands[] = "send";
@@ -83,23 +75,19 @@ $nsupdateCommands[] = "send";
 exec("echo \"" . implode("\n", $nsupdateCommands) . "\" | nsupdate", $output, $returnCode);
 
 // Check whether nsupdate responded with an error
-if ($returnCode)
-{
-	echo ERROR_DNSERROR;
-}
-else
-{
-	// Run post process command
-	if (isset($user->postprocess))
-	{
-		$commandLine = $user->postprocess;
-		$commandLine = str_replace("%username%", $username, $commandLine);
-		$commandLine = str_replace("%hostname%", $hostname, $commandLine);
-		$commandLine = str_replace("%ipaddress%", $ipAddress, $commandLine);
-		$commandLine = str_replace("%entrytype%", $entryType, $commandLine);
+if ($returnCode) {
+    echo ERROR_DNSERROR;
+} else {
+    // Run post process command
+    if (isset($user->postprocess)) {
+        $commandLine = $user->postprocess;
+        $commandLine = str_replace("%username%", $username, $commandLine);
+        $commandLine = str_replace("%hostname%", $hostname, $commandLine);
+        $commandLine = str_replace("%ipaddress%", $ipAddress, $commandLine);
+        $commandLine = str_replace("%entrytype%", $entryType, $commandLine);
 
-		exec($commandLine);
-	}
+        exec($commandLine);
+    }
 
-	echo ERROR_OK . " " . $ipAddress;
+    echo ERROR_OK . " " . $ipAddress;
 }
